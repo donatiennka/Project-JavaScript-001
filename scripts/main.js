bce9035406873b447e1b6e70d7617e130ea48771
donatech <donatien.nka@gmail.com>
2023-10-14 20:25:26 +0100

    Deuxième commit : écriture complète du code html de index.html; création et écriture du code CSS dans le fichier style.css; création et écriture du script de la fenêtre poppup dans le fichier popup.js; réecriture des fonctions du fichier script.js et en fin mise de lancerJeu en commentaire pur ne pas l'éxecuter et utilisation des méthodes getElemntBuId, querySelector et querySelectorAll dans le fichier main.js...

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,40 @@
+/*********************************************************************************
+ * 
+ * Point d'entrée, c'est lui qui intialise le jeu et lance la boucle de jeu. 
+ * 
+ *********************************************************************************/
 
-// On crée la fonction principale, c'est elle qui s'occupe de lancer toutes les autres fonctions
-function lancerJeu(){
-    let choixUtilisateur = choisirPhrasesOuMots()
-    let listeChoisi = chargerListe(choixUtilisateur)
-    let partieMessage = adapterMessage(choixUtilisateur)
-    const valRenvoyer = lancerBoucleDeJeu(listeChoisi, partieMessage)
-    resultat = afficherResultat(valRenvoyer[0], valRenvoyer[1])
-    console.log(resultat)
+//lancerJeu()
+ /**
+  * Selection grâce à la méthode getElementByld :
+  * - l'input dans lequel le joueur va écrire son texte
+  * - le bouton de validation
+  */
+
+let inputEcritureTexte = document.getElementById("inputEcriture")
+let boutonDeValidation = document.getElementById("btnValiderMot")
+console.log(inputEcritureTexte)
+console.log(boutonDeValidation)
+
+/**
+ * Sélection grâce à la méthode querySelector :
+ * - l'endroit où le mot proposé sera affiché
+ * - l'endroit où le score sera affiché 
+ */
+
+let zoneAffichageMot = document.querySelector(".zoneProposition")
+let zoneAffichageScore = document.querySelector(".zoneScore span")
+console.log(zoneAffichageMot)
+console.log(zoneAffichageScore)
+
+/**
+ * Sélection avec la méthode querySelectorAll :
+ * - les boutons radio de choix
+ */
+
+let lesBoutonsRadio = document.querySelectorAll("#mots, #phrases")
+console.log(lesBoutonsRadio)
+// affichage élément par élément
+for (let i = 0; i < lesBoutonsRadio.length; i++) {
+    console.log(lesBoutonsRadio[i])
 }
-// On lance le jeu en appelant la fonction principale
-lancerJeu()
